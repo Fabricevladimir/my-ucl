f2d4e874bf69af98c0b5f1a207e3f608dd94ba77
Fabricevladimir <fabricevladimir@gmail.com>
2020-11-21 11:49:29 -0500

    moved helper methods from stats page

---
+++ app/hooks/useApi.js
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+export default function useApi(apiFunction, initialState = {}) {
+  const [data, setData] = useState(initialState);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  async function request(...args) {
+    setLoading(true);
+    const response = await apiFunction(...args);
+    setLoading(false);
+
+    setError(!response.ok);
+    setData(response.data);
+    return response;
+  }
+
+  return { data, error, loading, request };
+}
