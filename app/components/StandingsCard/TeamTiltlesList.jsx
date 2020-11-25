73268da89d8b67081fd1b5be53da288e90d48757
Fabricevladimir <fabricevladimir@gmail.com>
2020-11-25 08:17:47 -0500

    added the list of titles to standing table

--- app/components/StandingsCard/TeamTiltlesList.jsx
+++ app/components/StandingsCard/TeamTiltlesList.jsx
@@ -1,9 +1,37 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Image, View } from 'react-native';
 
-// import Text from '../common/Text/Text';
+import Text from '../common/Text/Text';
 import styles from './TeamTitleList.styles';
+import colors from '../../styles/colors';
 
-export default function TeamTitlesList() {
-  return <View style={styles.container}></View>;
+const borderColors = {
+  1: { backgroundColor: colors.c1Blue },
+  2: { backgroundColor: colors.c1Blue },
+  3: { backgroundColor: colors.C2orange },
+  4: { backgroundColor: 'transparent', borderBottomWidth: 0 },
+};
+
+export default function TeamTitlesList({ teams }) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.label}>standings-title-list-header</Text>
+      </View>
+      <View style={styles.listContainer}>
+        {teams.map((team) => (
+          <View key={team.team_id} style={styles.row}>
+            <View style={[styles.designation, borderColors[team.rank]]} />
+            <Text style={styles.text}>{team.rank}</Text>
+            <View style={styles.logoContainer}>
+              <Image style={styles.logo} source={{ uri: team.logo }} />
+            </View>
+            <Text numberOfLines={1} style={[styles.text, styles.teamTitle]}>
+              {team.teamName}
+            </Text>
+          </View>
+        ))}
+      </View>
+    </View>
+  );
 }
--- app/components/StandingsCard/TeamTitleList.styles.js
+++ app/components/StandingsCard/TeamTitleList.styles.js
@@ -1,3 +1,45 @@
 import { StyleSheet } from 'react-native';
+import colors from '../../styles/colors';
 
-export default StyleSheet.create({});
+export default StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    width: '50%',
+  },
+  titleContainer: {
+    marginLeft: 13,
+  },
+  listContainer: {
+    width: '100%',
+    flexDirection: 'column',
+  },
+  row: {
+    height: 40,
+    flexDirection: 'row',
+  },
+  designation: {
+    width: 3,
+    marginRight: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: colors.white,
+  },
+  text: {
+    borderColor: colors.lightGray,
+    borderTopWidth: 1,
+    textAlignVertical: 'center',
+  },
+  logoContainer: {
+    width: 40,
+    alignItems: 'center',
+    borderColor: colors.lightGray,
+    borderTopWidth: 1,
+    justifyContent: 'center',
+  },
+  logo: {
+    width: 25,
+    height: 25,
+  },
+  teamTitle: {
+    flex: 1,
+  },
+});
