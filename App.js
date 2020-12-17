48aa62128930d2cd40ce820b7b4ba2c8a031a35a
Fabricevladimir <fabricevladimir@gmail.com>
2020-12-17 18:03:56 -0500

    removed local use-api hoook

--- App.js
+++ App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 
-import cache from './app/utils/cache';
 import logger from './app/utils/logger';
-import StandingsPage from './app/pages/StandingsPage';
+import StatsPage from './app/pages/StatsPage';
 import { LocaleProvider } from './app/contexts/LocaleContext';
 
 export default function App() {
   logger.init();
   return (
     <LocaleProvider>
-      <StandingsPage />
+      <StatsPage />
     </LocaleProvider>
   );
 }
